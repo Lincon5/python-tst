7d0706b8104ec8d867bda298062287a413c98957
Lincon5 <163899868+Lincon5@users.noreply.github.com>
2024-04-08 04:58:03 -0300

    código

--- ex007.py
+++ ex007.py
@@ -4,9 +4,9 @@ n1 = float(input('Primeira nota do aluno:'))
 n2 = float(input('Segunda nota do aluno:'))
 n3 = float(input('Terceira nota do aluno:'))
 n4 = float(input('Quarta nota do aluno:'))
-m = (n1 + n2 + n3 + n4) / 2
+m = (n1 * n2 + n3 ) **n4
 print('A média entre {:.1f} e {:.1F} e {:.1F} e {:.1f} é igual a {:.1f}'.format(n1, n2, n3, n4,  m))
 
+'''3 * 5 + 4 ** 2'''
 
-
-
+#(n1 + n2 + n3 + n4) / 2
