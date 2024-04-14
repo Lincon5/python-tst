6bd740c84631683b8ee47ec3ab2b7a53e36a4655
Lincon5 <163899868+Lincon5@users.noreply.github.com>
2024-04-14 04:14:21 -0300

    códigos

--- ex050.py
+++ ex050.py
@@ -4,9 +4,10 @@
 soma = 0
 cont = 0
 for c in range(1, 7):
-    num = int(input('Digite um{} número: '.format(c)))
-    soma += num
-    cont += 1
+    num = int(input('Digite um  {} valor: '.format(c)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
 print('Você informou {} números e a soma foi de {} '.format(cont, soma))
 
 
