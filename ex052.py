ceb17dbfeacc005c4c99d65863d31e4f76a91c82
Lincon5 <163899868+Lincon5@users.noreply.github.com>
2024-04-17 13:35:12 -0300

    códigos

--- ex052.py
+++ ex052.py
@@ -1,19 +1,16 @@
-#
+# Faça um programa que leia um númeor inteiro e diga se ele e primo ou não.
 
-num = int(input('Digoite um número:'))
+num = int(input('Digite um número:'))
 tot = 0
 for c in range(1, num + 1):
-    if num % c == 0:
+    if num % c ==0:
         print('\033[34m', end='')
         tot += 1
     else:
         print('\033[31m', end='')
-        print('{}'.format(c), end='')
-print('\n\033[mO número {} foi divisível {} vezes'.format(num, tot))
+    print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divisível {} vezes.'.format(num, tot))
 if tot == 2:
-    print('\033[35mEle vai ser PRIMO.\033[m')
+    print('\033[35mEle vai ser primo.\033[m')
 else:
-    print('\033[31mEle não vai ser PRIMO.\033[m')
-
-
-# \n vai ser para quebrar linha.
+    print('\033[31mEle não vai ser primo.\033[m')
