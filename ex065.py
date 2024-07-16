957f0d75ed538331e9d29ec4abab498fa29fe073
Lincon5 <163899868+Lincon5@users.noreply.github.com>
2024-07-16 00:40:44 -0300

    códigos

---
+++ ex065.py
@@ -0,0 +1,22 @@
+resp = 'S'
+soma = 0
+quant = 0
+media = 0
+maior = 0
+menor = 0
+while resp in 'Ss':
+    n = int(input('Digite um número: '))
+    soma += n
+    quant += 1
+    if quant == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor =  n
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+média = soma / quant
+print('Você digitou {} números e a média foi {}'.format(quant, média))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
+
